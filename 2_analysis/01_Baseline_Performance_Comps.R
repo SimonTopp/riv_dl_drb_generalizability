d72d791da65d44135f1e6f062bc88f81d7760222
Simon Topp <33098707+SimonTopp@users.noreply.github.com>
2022-09-27 17:07:21 -0400

    Streamlined files, removed redundancies, standardized some naming

--- 2_analysis/01_Baseline_Performance_Comps.R
+++ 2_analysis/01_Baseline_Performance_Comps.R
@@ -1,10 +1,7 @@
 library(tidyverse)
 library(lubridate)
-library(reticulate)
 library(sf)
-library(plotly)
 library(feather)
-library(ggridges)
 library(ggpubr)
 library(knitr)
 library(kableExtra)
@@ -38,47 +35,9 @@ gwn_reps <- read_replicates('results/baseline/GWN/full_train', 'overall_metrics'
 rgcn_reps <- read_replicates('results/baseline/RGCN/full_train', 'overall_metrics') %>%
   filter(partition=='tst')## Best, 2
 
-### function for comparing across replicates
-replicate_comps <- function(run1,run2, scenario){
-  run_1 <- read_replicates(run1, 'overall_metrics')
-  run_2 <- read_replicates(run2, 'overall_metrics')
-  sig_test<-t.test(run_1$rmse[run_1$partition=='tst'],run_2$rmse[run_2$partition=='tst']) %>%
-    broom::tidy() %>% mutate(group='overall') %>%
-    bind_rows(t.test(run_1$rmse_top10[run_1$partition=='tst'],run_2$rmse_top10[run_2$partition=='tst']) %>%
-                broom::tidy() %>% mutate(group='warmest10')) %>%
-    mutate(scenario = scenario)
-  return(sig_test)
-}
-
-replicate_sigs <- replicate_comps('results/baseline/GWN/full_train','results/baseline/RGCN/full_train', 'baseline') %>%
-  bind_rows(replicate_comps('results/LLO/coastal/GWN/full_train','results/LLO/coastal/RGCN/full_train', 'coastal')) %>%
-  bind_rows(replicate_comps('results/LLO/appalachians/GWN/full_train','results/LLO/appalachians/RGCN/full_train', 'appalachians')) %>%
-  bind_rows(replicate_comps('results/LLO/piedmont/GWN/full_train','results/LLO/piedmont/RGCN/full_train', 'headwaters')) %>%
-  bind_rows(replicate_comps('results/LTO/max/GWN/full_train','results/LTO/max/RGCN/full_train', 'train cold/test hot')) %>%
-  bind_rows(replicate_comps('results/LTO/min/GWN/full_train','results/LTO/min/RGCN/full_train', 'train hot/test cold')) %>%
-  bind_rows(replicate_comps('results/Drought/GWN/full_train','results/Drought/RGCN/full_train', 'Drought')) %>%
-  mutate(best = ifelse(estimate1<estimate2, "GWN","RGCN"))
-
-overall_sigs <- replicate_sigs %>% filter(group == 'overall',p.value < 0.05)
-warmest_sigs <- replicate_sigs %>% filter(group == 'warmest10',p.value < 0.05)
-
-shapiro.test(rgcn_reps$rmse[rgcn_reps$partition=='tst'])
-
-check <- t.test(gwn_reps$rmse[gwn_reps$partition=='tst'],rgcn_reps$rmse[rgcn_reps$partition=='tst'])
-
-t.test(gwn_reps$rmse_top10[gwn_reps$partition=='tst'],rgcn_reps$rmse_top10[rgcn_reps$partition=='tst'])
-
-check <- t.test(rgcn_reps$rmse[rgcn_reps$partition=='tst']) %>% broom::tidy() %>%
-  mutate(model = 'rgcn') %>%
-  bind_rows(t.test(rgcn_reps$rmse[rgcn_reps$partition=='tst']) %>% broom::tidy() %>%
-              mutate(model = 'gwn'))
-
-ggplot(check, aes(x = model, y = estimate))+
-  geom_point()+
-  geom_errorbar(aes(ymin = conf.low, ymax= conf.high))
-t.test(gwn_reps$rmse[gwn_reps$partition=='tst'],rgcn_reps$rmse[rgcn_reps$partition=='tst'], alternative='less')
-
-## Figure for overall difference between the two models
+##########
+### Summary baseline comparison figure
+#########
 
 p1 <- gwn_stats$segs %>% mutate(run = 'GWN') %>% bind_rows(rgcn_stats$segs %>% mutate(run = 'RGCN')) %>%
   filter(train_type == "Full Train") %>%
@@ -89,29 +48,12 @@ p1 <- gwn_stats$segs %>% mutate(run = 'GWN') %>% bind_rows(rgcn_stats$segs %>% m
 
 p1
 
+### Calculate Difference between baseline runs
 baseline_diff <- seg_error_diff(rgcn_stats$segs, gwn_stats$segs, 'RGCN', 'GWN',shape=edges)
-
-
-###Take a look at how performance varies with reach type
-res_info <- readRDS('data_DRB/DRB_spatial/segments_relative_to_reservoirs.rds')
-res_metrics <- baseline_diff %>% left_join(res_info) %>%
-  mutate(res_group = if_else(type_res %in% c("contains_reservoir","within_reservoir", "downstream of reservoir (1)","reservoir_inlet_reach","downstream of reservoir (2)", "reservoir_outlet_reach"), 'Impacted','Not Impacted')) %>%
-  group_by(res_group,metric) %>%
-  summarise(RGCN = median(RGCN, na.rm=T),
-            GraphWaveNet = median(GWN, na.rm=T),
-            count = n())
-
-res_metrics <- baseline_diff %>% left_join(res_info) %>%
-  group_by(type_res, metric) %>%
-  summarise(RGCN = median(RGCN, na.rm=T),
-            GraphWaveNet = median(GWN, na.rm=T),
-            count = n())
 
 p2 <- baseline_diff %>% filter(metric == 'rmse_mean') %>% seg_plotter_sf(., 'metric_diff',ll=-1,ul=2, diff = T, network_color = 'white')+
   labs(title = 'RGCN minus GWN',subtitle='Blue=GWN outperformed RGCN', color = 'RMSE\nDifference') +
   theme(title = element_text(size =8))
-
-
 
 p3 <- rgcn_stats$months %>% filter(partition == 'tst',train_type=='Full Train') %>% mutate(model= 'RGCN') %>%
   bind_rows(gwn_stats$months %>% filter(partition == 'tst',train_type=='Full Train') %>% mutate(model = 'GWN')) %>%
@@ -138,6 +80,24 @@ g <- gridExtra::grid.arrange(p1,p2,p3, layout_matrix = layout)
 
 ggsave('../drb_gwnet/2_analysis/figures/baseline_comps.png', plot = g, width = 5, height = 5, units = 'in')
 
+
+
+############
+#### Look at how performance varies across reservoir and non-reservoir sites
+###########
+res_info <- readRDS('data_DRB/DRB_spatial/segments_relative_to_reservoirs.rds')
+
+res_metrics <- baseline_diff %>% left_join(res_info) %>%
+  mutate(res_group = if_else(type_res %in% c("contains_reservoir","within_reservoir", "downstream of reservoir (1)","reservoir_inlet_reach","downstream of reservoir (2)", "reservoir_outlet_reach"), 'Impacted','Not Impacted')) %>%
+  group_by(res_group,metric) %>%
+  summarise(RGCN = median(RGCN, na.rm=T),
+            GraphWaveNet = median(GWN, na.rm=T),
+            count = n())
+res_metrics %>% filter(metric=='rmse_mean')
+res_metrics %>% filter(metric=='nse_mean')
+
+
+##### Summary tables for presentations
 rgcn_stats$temps %>% mutate(model = 'RGCN') %>%
   bind_rows(gwn_stats$temps %>% mutate(model = 'GWN')) %>%
   filter(partition == 'tst',
@@ -172,6 +132,7 @@ rgcn_stats$temps %>% mutate(model = 'RGCN') %>%
   rename(`Temperature Bin` = name) %>%
   pivot_wider(names_from = model, values_from = value) %>%
   mutate(`GWN Improvement` = GWN-RGCN) %>%
+  mutate_if(is.numeric,round,digits=3) %>%
   relocate(RGCN, .after = GWN) %>%
   kableExtra::kbl(col.names = c('Temperature\nBin','GWN','RGCN','Difference(GWN-RGCN)')) %>%
   kableExtra::add_header_above(c('Overall NSE by Temperature Bin' = 4)) %>%
@@ -179,37 +140,3 @@ rgcn_stats$temps %>% mutate(model = 'RGCN') %>%
   kableExtra::row_spec(3, bold = T, color = 'white', background = '#528AAE') %>%
   kableExtra::row_spec(1, bold = T, color = 'grey') %>%
   kableExtra::kable_material(full_width = F)
-
-
-p1 <- rgcn_stats$temps %>% 
-  reshape_metric(.,'rmse',c('partition','run','model')) %>% mutate(model = 'RGCN') %>%
-  bind_rows(gwn_stats$temps %>% reshape_metric(.,'rmse',c('partition','run','model')) %>% mutate(model='GWN')) %>%
-  filter(partition =='tst') %>%
-  ggplot(.,aes(x = group, y = mean, color = model)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd)) +
-  theme_bw() +
-  ggtitle('RMSE')
-
-p2 <- rgcn_stats$temps %>%
-  reshape_metric(.,'nse',c('partition','run','model')) %>% mutate(model = 'RGCN') %>%
-  bind_rows(gwn_stats$temps %>% reshape_metric(.,'nse',c('partition','run','model')) %>% mutate(model='GWN')) %>%
-  filter(partition =='tst') %>%
-  ggplot(.,aes(x = group, y = mean, color = model)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd)) +
-  theme_bw() +
-  ggtitle('nse')
-
-gridExtra::grid.arrange(p1,p2, ncol = 1, top = 'Mean and SD across replicates')
-
-
-### Plot mean and sd
-full_temps %>% filter(partition == 'tst') %>%
-  reshape_metric(.,'rmse',c('partition','run','model')) %>%
-  ggplot(.,aes(x = group, y = mean, color = model)) +
-  geom_point() +
-  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd)) +
-  theme_bw() +
-  ggtitle('rmse') +
-  facet_wrap(~run, scales = 'free')
--- 2_analysis/utils.R
+++ 2_analysis/utils.R
@@ -1,66 +1,15 @@
+############# 
+###Various functions to help aggregate and analyze results from across model runs
+###and XAI experiments
+######
+
+
+#### Generic reader function to read csv based on keywords
 reader <- function(id, files){
   df <- read_csv(grep(paste0(id,'/'), files, value = T),show_col_types = FALSE) %>% mutate(run = id)
 }
 
-view_test_results <- function(folder){
-  files <- list.files(folder, full.names = T, recursive = T, pattern = 'test_results.csv')
-  
-  runs <- list.dirs(folder, recursive =F) %>% map(~str_split(., pattern ='/') %>% flatten() %>% last(.)) %>% unlist()
-  train_logs <- runs %>% map_dfr(reader, files = files)
-}
-
-
-view_train_longs <- function(folder,pretrain=F){
-  
-  if(pretrain==T){
-    files <- list.files(folder, full.names = T, recursive = T, pattern = 'pretrain_log.csv')
-    runs <- c(0:9)
-    train_logs <- map2_dfr(files,runs, ~read_csv(.x) %>% mutate(run=.y))
-  }else if(pretrain==F){
-    files <- list.files(folder, full.names = T, recursive = T, pattern = 'finetune_log.csv')
-  
-    runs <- list.dirs(folder, recursive =F) %>% map(~str_split(., pattern ='/') %>% flatten() %>% last(.)) %>% unlist()
-  
-    train_logs <- runs %>% map_dfr(reader, files = files)
-  }
-  return(train_logs)
-}
-
-rmse_by_seg <- function(id, files, model){
-#browser()
-    if(model=='rgcn'){
-    df <- reader(id, files) %>%
-      rename(temp_pred = pred, temp_ob=obs) %>%
-      filter(!is.na(temp_ob)) %>%
-      group_by(seg_id_nat) %>%
-      summarize(count = n(),
-                rmse=Metrics::rmse(temp_ob,temp_pred)) %>%
-      mutate(run = id)
-      
-  }else{
-    df <- reader(id, files) %>% filter(!is.na(temp_ob)) %>%
-      group_by(seg_id_nat) %>%
-      summarize(count = n(),
-                rmse=Metrics::rmse(temp_ob,temp_pred),
-                mean_ci = mean(ci_high-ci_low),
-                std_ci = sd(ci_high-ci_low)) %>%
-      mutate(run = id)
-  }
-
-}  
-  
-view_rmse_by_seg <- function(folder, model){
-  
-  files <- list.files(folder, full.names = T, recursive = T, pattern = 'test_results.csv')
-  
-  runs <- list.dirs(folder, recursive =F) %>% map(~str_split(., pattern ='/') %>% flatten() %>% last(.)) %>% unlist()
-  
-  run_summaries <- runs %>% map_dfr(rmse_by_seg, files = files, model=model)  
-  
-  return(run_summaries)
-}
-
-
+#### Plot up results spatially
 seg_plotter_sf <- function(log, var, part = 'tst', ll = NA, ul =NA, diff = F, network_color = 'grey80'){
   spatial <- readRDS('../river-dl/data_DRB/DRB_spatial/network.rds')
   edges <- spatial$edges %>% st_as_sf()
@@ -98,24 +47,7 @@ seg_plotter_sf <- function(log, var, part = 'tst', ll = NA, ul =NA, diff = F, ne
   } 
   }
 
-seg_rmse_diff <- function(log1,log2,name1, name2, part = 'tst'){
- # browser()
-  spatial <- readRDS('data/in/DRB_spatial/network.rds')
-  edges <- spatial$edges %>% st_as_sf()
-  
-  full_log <- log1 %>% filter(partition == part, !is.na(rmse_mean)) %>% full_join(edges %>% st_set_geometry(NULL)) %>% mutate(comp = name1) %>%
-    bind_rows(log2 %>% filter(partition == part, !is.na(rmse_mean)) %>% full_join(edges %>% st_set_geometry(NULL)) %>% mutate(comp = name2)) 
-  
-  diff <- full_log %>% 
-    select(comp,seg_id_nat, rmse_mean) %>% 
-    filter(!is.na(seg_id_nat)) %>% 
-    pivot_wider(names_from=comp, values_from = rmse_mean) %>%
-    mutate(rmse_diff = (!!sym(name1))-(!!sym(name2))) %>%
-  
-
-  return(diff)
-}
-
+#### Calculate difference in error metrics by segment between two model runs
 seg_error_diff <- function(log1,log2,name1, name2, part = 'tst', run_type = 'Full Train', shape){
 
   full_log <- log1 %>% filter(partition == part, train_type==run_type) %>% 
@@ -140,67 +72,8 @@ seg_error_diff <- function(log1,log2,name1, name2, part = 'tst', run_type = 'Ful
     return(diff)
 }
 
-### Pull out summary stats
-seg_sum <- function(df){
-  df %>% group_by(seg_id_nat, partition) %>%
-    summarize(count = n(),
-              rmse=Metrics::rmse(temp_ob,temp_pred),
-              bias = Metrics::bias(temp_pred,temp_ob))
-}          
 
-month_sum <- function(df){
-  df %>% mutate(month = lubridate::month(date, label = T)) %>% 
-    group_by(month, partition) %>%
-    summarize(count = n(),
-              rmse=Metrics::rmse(temp_ob,temp_pred),
-              bias = Metrics::bias(temp_pred,temp_ob))
-} 
-
-temp_sum <- function(df){
-  grouped <- df %>% mutate(temp_group = ifelse(temp_ob > 20, 'Above 20', ifelse(temp_ob < 10, 'Below 10', 'Normal Temp'))) %>% 
-    group_by(temp_group, partition) %>%
-    summarize(count = n(),
-              rmse=Metrics::rmse(temp_ob,temp_pred),
-              bias = Metrics::bias(temp_pred,temp_ob))
-  grouped %>% bind_rows(df %>% group_by(partition) %>%summarise(count= n(), 
-                                         rmse = Metrics::rmse(temp_ob,temp_pred),
-                                         bias = Metrics::bias(temp_pred,temp_ob)) %>%
-                          mutate(temp_group = 'Overall'))
-}
-
-sum_all <- function(id, files, model, llo){
-  #browser()
-  if(model=='rgcn'){
-    df <- reader(id, files) %>%
-      rename(temp_pred = pred, temp_ob=obs) %>%
-      filter(!is.na(temp_ob))
-  }else{
-    df <- reader(id, files) %>% filter(!is.na(temp_ob))
-  }
-  
-  if(llo){
-    llo_groups <- read_csv('../data/in/DRB_spatial/llo_groups.csv') %>%
-      mutate(run = paste0('group_',test_group)) %>% select(run, seg_id_nat)
-    df <- df %>% inner_join(llo_groups)
-  }
-  segs <- seg_sum(df) %>% mutate(run = id)
-  months <- month_sum(df) %>% mutate(run = id)
-  temps <- temp_sum(df) %>% mutate(run = id)
-  #rm(df,id, files, model)
-  return(mget(c('segs', 'months','temps')))
-}
-
-summary_stats <- function(folder, model, llo = F){
-  #browser()
-  files <- list.files(folder, full.names = T, recursive = T, pattern = 'test_results.csv')
-  
-  runs <- list.dirs(folder, recursive =F) %>% map(~str_split(., pattern ='/') %>% flatten() %>% last(.)) %>% unlist()
-  
-  run_summaries <- runs %>% map(sum_all, files = files, model=model, llo=llo)  
-  
-  return(run_summaries)
-}
-
+### Function for combining replicate model runs
 combine_replicates = function(folder, pattern,subfolders = F){
   
   files <- list.files(folder, pattern=pattern, recursive=T, full.names=T)
@@ -253,83 +126,6 @@ combine_replicates = function(folder, pattern,subfolders = F){
 }
 
 
-add_bias <- function(folder){
-  files <- list.files(folder, pattern='combined_results', recursive=T, full.names=T)
-  for(i in c(1:length(files))){
-    df <- read_csv(files[i]) %>% rename(temp_ob = observed, temp_pred = predicted) %>%
-      filter(!is.na(temp_ob))
-    if(i == 1){
-      segs <- seg_sum(df) %>% mutate(replicate = i)
-      months <- month_sum(df) %>% mutate(replicate = i)
-      temps <- temp_sum(df) %>% mutate(replicate = i)
-      }else{
-        segs <- segs %>% bind_rows(seg_sum(df) %>% mutate(replicate = i))
-        months <- months %>% bind_rows(month_sum(df) %>% mutate(replicate = i))
-        temps <- temps %>% bind_rows(temp_sum(df) %>% mutate(replicate = i))
-    }
-  }
-  segs <- segs %>% group_by(across(!c(rmse,bias,replicate))) %>%
-    summarise(across(everything(),c(mean = mean,sd = sd),.names = "{.col}_{.fn}")) %>%
-    select(-c(replicate_mean, replicate_sd))
-  months <- months %>% group_by(across(!c(rmse,bias,replicate))) %>%
-    summarise(across(everything(),c(mean = mean,sd = sd),.names = "{.col}_{.fn}")) %>%
-    select(-c(replicate_mean, replicate_sd))
-  temps <- temps %>% group_by(across(!c(rmse,bias,replicate))) %>%
-    summarise(across(everything(),c(mean = mean,sd = sd),.names = "{.col}_{.fn}")) %>%
-    select(-c(replicate_mean, replicate_sd))
-  return(mget(c('segs', 'months','temps')))
-}
-
-
-pinsploder <- function(folder, pattern, st = F){
-  files <- list.files(folder, pattern=pattern, recursive=T, full.names=T)
-  if(st){
-    files = grep('/st/',files, value = T)
-  }
-  for(i in c(1:length(files))){
-    pt_es <- str_split(files[i], '/', simplify = T)
-    pt_es <- grep("^[0-9]*_[0-9]*$", pt_es, value = T)
-    pt <- str_split(pt_es,'_',simplify = T)[1]
-    es <- str_split(pt_es,'_',simplify = T)[2]
-    if(i == 1){
-      df <- read_csv(files[i]) %>%
-        mutate(pt_epochs = pt, early_stopping = es)
-    }else{
-      df <- df %>% bind_rows(read_csv(files[i]) %>%
-                               mutate(pt_epochs = pt, early_stopping = es))
-    }
-  }
-  df <- df %>% mutate(pt_epochs = factor(as.integer(pt_epochs)),
-               early_stopping = factor(as.integer(early_stopping)))
-  return(df)
-}
-
-rdl_htsploder <- function(folder, pattern, model="GWN"){
-  files <- list.files(folder, pattern=pattern, recursive=T, full.names=T)
-  files <- grep(model, files, value = T)
-  for(i in c(1:length(files))){
-    pt_es <- str_split(files[i], '/', simplify = T)
-    pt_es <- grep("[0-9]_[0-9]*$", pt_es, value = T)
-    pt <- str_split(pt_es,'_',simplify = T)[1]
-    es <- str_split(pt_es,'_',simplify = T)[2]
-    if(i == 1){
-      df <- read_csv(files[i], na=c('','NA','nan')) %>%
-        mutate(offset = pt, seq_len = es)
-    }else{
-      df <- df %>% bind_rows(read_csv(files[i],na=c('','NA','nan')) %>%
-                               mutate(offset = pt, seq_len = es))
-    }
-  }
-  df <- df %>% mutate(offset = factor(offset),
-                      #recurrent_dropout = factor(seq_len),
-                      seq_len = factor(as.integer(seq_len)))
-                      #dropout = factor(offset))
-  return(df)
-}
-
-
-
-
 ### We need a helper function to reshape data conveniently
 reshape_metric <- function(log, metric,keep_vars= c('partition'),difference=FALSE){
   cols_mean <- paste(metric,c('mean','top10_mean','bot10_mean'), sep ='_')
@@ -370,7 +166,7 @@ reshape_metric <- function(log, metric,keep_vars= c('partition'),difference=FALS
   }
 }
 
-
+### Read in Replicates without summarizing
 read_replicates = function(folder, pattern,subfolders = F){
   
   files <- list.files(folder, pattern=pattern, recursive=T, full.names=T)
@@ -397,7 +193,7 @@ read_replicates = function(folder, pattern,subfolders = F){
   return(df)
 }
 
-
+#### Aggregate XAI results into a dataframe
 aggregate_xai <- function(folder, pattern){
   
   files <- list.files(folder, pattern=pattern, recursive=T, full.names=T)
@@ -407,10 +203,8 @@ aggregate_xai <- function(folder, pattern){
   return(df)
 }
 
-###
-###### Compare significance across replicate runs
-###
-### function for comparing across replicates
+
+### Function for comparing across replicates
 replicate_comps <- function(run1,run2, scenario){
   run_1 <- read_replicates(run1, 'overall_metrics')
   run_2 <- read_replicates(run2, 'overall_metrics')
@@ -431,8 +225,7 @@ replicate_comps <- function(run1,run2, scenario){
 
 
 
-### Try to get mean and sd changes from baseline for both models
-
+### Pull mean and sd of changes from baseline for both models
 compare_replicate_pairs <- function(baseline, scenario, scenario_label, metric='rmse'){
   
   run_1 <- read_replicates(baseline, 'overall_metrics') %>%
